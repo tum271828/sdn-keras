eb34269c755667c622cf431924c82c660936666f
tum271828 <tum2.71828@gmail.com>
2019-01-31 10:31:28 +0700

    clean code

--- train.py
+++ train.py
@@ -1,16 +1,24 @@
 import sdn
 import util
 from keras.callbacks import *
+from gpu_stat import *
+import time
 
-sdn=sdn.SDN(3)
 
+trainImagePath="/your_path/train_img"
+trainMaskPath="/your_path/train_mask"
+testImagePath="/your_path/test_img"
+testMaskPath="/your_path/test_mask"
+batchSize=4
+nImg=398
 useHierarchicalSupervision=False
+useScoreMapConnect=True
+
 if useHierarchicalSupervision:
-    logger=CSVLogger("log.hsv.csv",append=True)
-    g=util.flowFromPath("/your-path/train_img",
-    "/your-path/train_mask",1,6)   
-    g2=util.flowFromPath("/your-path/test_img",
-    "/your-path/test_mask",1,6)   
+    sdn=sdn.SDN(3,useScoreMapConnect=useScoreMapConnect)
+    logger=CSVLogger("log.hsv{}.csv".format(time.time()),append=True)
+    g=util.flowFromPath(trainImagePath,trainMaskPath,batchSize,6,sdn.preprocess)   
+    g2=util.flowFromPath(trainImagePath,trainMaskPath,batchSize,6,sdn.preprocess,shuffle=False)   
     lr=ReduceLROnPlateau(monitor='output_loss',factor=0.1,patience=3,verbose=1,mode='min',cooldown=0,min_lr= 0)
     stop=EarlyStopping(monitor='output_loss', min_delta=0, patience=5, verbose=0, mode='min')
     loss={'output': "categorical_crossentropy", 
@@ -22,20 +30,22 @@ if useHierarchicalSupervision:
         'softmax_1_0': 1,
         'softmax_2_0': 1,'softmax_2_1': 1}
     sdn.trainModel.compile(loss=loss,loss_weights=lossWeight,optimizer="adam",
-            metrics=['accuracy'])
+        metrics=['accuracy'])
+    sdn.trainModel.fit_generator(g, nImg//batchSize, 50, callbacks=[logger,lr,stop],
+        validation_data=g2, validation_steps=nImg//batchSize)
+    sdn.trainModel.save_weights("weight.hsv_smap.hdf5", overwrite=True)
 
-    sdn.trainModel.fit_generator(g, 398, 25,callbacks=[logger,lr,stop],validation_data=g2, validation_steps=398)
-    sdn.trainModel.save_weights("weight.hsv.hdf5", overwrite=True)
 else:
-    logger=CSVLogger("log.no_hsv.csv",append=True)
-    g=util.flowFromPath("/your-path/train_img",
-    "/your-path/train_mask",1,6)   
-    g2=util.flowFromPath("/your-path/test_img",
-    "/your-path/test_mask",1,6)   
+    sdn=sdn.SDN(3)
+    logger=CSVLogger("log.no_hsv{}.csv".format(time.time()),append=True)
+    g=util.flowFromPath(trainImagePath,trainMaskPath,batchSize,1,sdn.preprocess)   
+    g2=util.flowFromPath(trainImagePath,trainMaskPath,batchSize,1,sdn.preprocess,shuffle=False)   
     lr=ReduceLROnPlateau(monitor='loss',factor=0.1,patience=3,verbose=1,mode='min',cooldown=0,min_lr= 0)
     stop=EarlyStopping(monitor='loss', min_delta=0, patience=5, verbose=0, mode='min')
     sdn.model.compile(loss="categorical_crossentropy",optimizer="adam",
-            metrics=['accuracy'])
+        metrics=['accuracy'])
 
-    sdn.model.fit_generator(g, 398, 25,callbacks=[logger,lr,stop],validation_data=g2, validation_steps=398)
+    sdn.model.fit_generator(g, nImg//batchSize, 50,callbacks=[logger,lr,stop],
+        validation_data=g2, validation_steps=nImg/batchSize)
     sdn.model.save_weights("weight.no_hsv.hdf5", overwrite=True)
+
